0331d86d56ea7deafbacafc06ade0447c9435cc4
markbugatti <supermark11@gmail.com>
2016-09-26 11:31:28 +0300

    commit

---
+++ one.js
@@ -0,0 +1,11 @@
+
+/**
+ * Created by Admin on 24.09.16.
+ */
+
+
+//var stavka = document.getElementById("Stavka").value;
+
+function StavkaFunc(tmp){
+    alert ("ваша ставка: " + tmp + "грн, ждите матч");
+}
